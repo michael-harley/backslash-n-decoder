72b9d9fdbfdc5365823f0c7aceec39c4306d3310
michael-harley <michael.harley@traveloka.com>
2019-11-04 18:31:07 +0700

    Finalize website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import { TextArea, Form, Segment, Header, Divider } from "semantic-ui-react";
+
+import styles from "./App.module.css";
 
 function App() {
+  const [decodeValue, setDecodeValue] = useState("");
+  console.log(decodeValue.split("\\n"));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <Header as="h1" className={styles.title}>
+        Welcome to \n decoder
+      </Header>
+      <Form>
+        <strong>Input value to decode:</strong>{" "}
+        <TextArea
+          placeholder="Input value to decode"
+          value={decodeValue.join}
+          onChange={(e, { value }) => setDecodeValue(value)}
+        />
+      </Form>
+      {decodeValue && (
+        <Segment className={styles.result}>
+          <Header as="h3" textAlign="center">
+            Result:
+          </Header>
+          <Divider />
+          {decodeValue.split("\\n").map((v, idx) => {
+            const tabLength = 4;
+            const tabs = v.split("\\t").length - 1;
+
+            return (
+              <div key={idx} style={{ paddingLeft: `${tabLength * tabs}em` }}>
+                {v.replace(/\\t/g, "")}
+                <br />
+              </div>
+            );
+          })}
+        </Segment>
+      )}
+      <div className={styles.footer}>by Michael Harley</div>
     </div>
   );
 }
